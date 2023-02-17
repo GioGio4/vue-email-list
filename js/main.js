86e5e9ea0b55e986de111f6cd415ed8814b11e7e
GioGio4 <g.ceccobelli@outlook.it>
2023-02-17 17:20:41 +0100

    Add style css + fix (js reset email list click)

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ createApp({
   },
   methods: {
     GenerateRandomListEmail() {
+      this.randomEmailList = [];
+      this.completeList = 0;
       for (let i = 0; i < 10; i++) {
         axios
           .get("https://flynn.boolean.careers/exercises/api/random/mail")

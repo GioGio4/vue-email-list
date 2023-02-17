dd6530cc7c697e50c4a59d42042ac2c0999dafcf
GioGio4 <g.ceccobelli@outlook.it>
2023-02-17 16:41:07 +0100

    Add js code (bonus)

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ createApp({
   data() {
     return {
       randomEmailList: [],
+      completeList: 0,
     };
   },
   methods: {
@@ -13,6 +14,7 @@ createApp({
           .get("https://flynn.boolean.careers/exercises/api/random/mail")
           .then((result) => {
             this.randomEmailList.push(result.data.response);
+            this.completeList++;
           });
       }
     },
